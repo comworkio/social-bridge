ec3890a57b044206f6c00ee75bcee6bfec3ad833
Idriss Neumann <idriss.neumann@comwork.io>
2022-10-27 12:15:05 +0100

    Multiple slack token

--- utils/logger.py
+++ utils/logger.py
@@ -2,26 +2,31 @@ import os
 import pycurl
 import json
 
-from utils.common import is_not_empty
+from utils.common import is_empty
 
 LOG_LEVEL = os.environ['LOG_LEVEL']
 SLACK_TRIGGER = os.environ['SLACK_TRIGGER']
 
-def slack_message( message , is_public):
+def slack_message ( message , token):
     if SLACK_TRIGGER == 'on':
         c = pycurl.Curl()
-
-        token = os.environ['SLACK_TOKEN']
-        if is_public and is_not_empty(os.getenv('SLACK_TOKEN_PUBLIC')):
-            slack_message(message, False)
-            token = os.environ['SLACK_TOKEN_PUBLIC']
-
         c.setopt(pycurl.URL, "https://hooks.slack.com/services/{}".format(token))
         c.setopt(pycurl.HTTPHEADER, ['Accept: application/json'])
         c.setopt(pycurl.POST, 1)
         data = json.dumps({"text": message, "username": os.environ['SLACK_USERNAME'], "channel": os.environ['SLACK_CHANNEL'], "icon_emoji": os.environ['SLACK_EMOJI'] })
         c.setopt(pycurl.POSTFIELDS, data)
         c.perform()
+
+def slack_messages( message , is_public):
+        slack_message(message, os.environ['SLACK_TOKEN'])
+        if is_public:
+            i = 1
+            while True:
+                token_val = os.getpid("SLACK_PUBLIC_TOKEN_{}".format(i))
+                if is_empty(token_val):
+                    break
+                slack_message(message, token_val)
+                i = i+1
 
 def check_log_level ( log_level ):
     if LOG_LEVEL == "debug" or LOG_LEVEL == "DEBUG":
@@ -36,4 +41,4 @@ def quiet_log_msg ( log_level, message ):
 def log_msg( log_level, message, is_public = False):
     if check_log_level(log_level):
         quiet_log_msg (log_level, message)
-        slack_message(message, is_public)
+        slack_messages(message, is_public)
