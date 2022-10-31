66bfdd69652189df08f26487e67ddf21a99ff380
Idriss Neumann <idriss.neumann@comwork.io>
2022-10-31 17:58:57 +0100

    Add authentication method

--- utils/uprodit.py
+++ utils/uprodit.py
@@ -1,3 +1,4 @@
+from abc import get_cache_token
 from hashlib import sha1
 from uuid import uuid4
 from time import time
@@ -7,6 +8,12 @@ from urllib.parse import quote
 import hmac
 import re
 import os
+import requests
+import bcrypt
+
+from utils.common import is_empty, is_not_empty, is_not_empty_array, sn_message
+from utils.logger import log_msg
+from utils.redis import set_cache_value
 
 UPRODIT_API_URL = os.environ['UPRODIT_API_URL']
 UPRODIT_APPID = os.getenv('UPRODIT_APPID')
@@ -15,7 +22,10 @@ UPRODIT_USERNAME = os.getenv('UPRODIT_USERNAME')
 UPRODIT_PASSWORD = os.getenv('UPRODIT_PASSWORD')
 UPRODIT_PROFILE_ID = os.getenv('UPRODIT_PROFILE_ID')
 
-UPRODIT_NEWSPAPER_API = "{}/v1/post"
+UPRODIT_TOKEN_API = "{}/v1/token".format(UPRODIT_API_URL)
+UPRODIT_NEWSPAPER_API = "{}/v1/post".format(UPRODIT_API_URL)
+
+UPRODIT_CACHE_TOKEN_KEY = "uprodit_token_key"
 
 def hmacsha1(key, raw):
     hashed = hmac.new(str.encode(key), str.encode(raw), sha1)
@@ -32,4 +42,32 @@ def generate_signature (appid, env, uri):
     auth_timestamp = time()
     return "Auth ?auth_signature={}&auth_nonce={}&auth_callback={}&auth_timestamp={}&auth_token={}&auth_signature_method={}&auth_consumer_key={}".format(auth_signature, auth_nonce, auth_callback, auth_timestamp, auth_token, auth_signature_method, auth_consumer_key)
 
-def send_uprodit(username, message):
+def get_token():
+    token = get_cache_token(UPRODIT_CACHE_TOKEN_KEY)
+    if is_not_empty(token):
+        return token
+
+    payload = {"username": UPRODIT_USERNAME, "password": UPRODIT_PASSWORD}
+    headers = {"Authorization": generate_signature(UPRODIT_APPID, UPRODIT_ENV, UPRODIT_TOKEN_API)}
+    r = requests.post(UPRODIT_TOKEN_API, json = payload, headers = headers)
+    if r.status_code != 200:
+        log_msg("ERROR", "[get_token] authentication error : username = {}, code = {}".format(UPRODIT_USERNAME, r.status_code))
+        return None
+
+    token = r.json()['token']
+    set_cache_value(UPRODIT_CACHE_TOKEN_KEY, token)
+    return token
+
+def send_uprodit(username, message, urls):
+    content = sn_message(username, message)
+    payload = {"idProfilPost": UPRODIT_PROFILE_ID, "descriptionPost": content}
+    uprodit_token = get_token()
+
+    if is_empty(uprodit_token):
+        return
+
+    if is_not_empty_array(urls):
+        payload['link'] = urls[0]
+
+    headers = {"Authorization": generate_signature(UPRODIT_APPID, UPRODIT_ENV, UPRODIT_NEWSPAPER_API), "x-uprodit-token": uprodit_token}
+    requests.post(UPRODIT_NEWSPAPER_API, json = payload, headers = headers)
