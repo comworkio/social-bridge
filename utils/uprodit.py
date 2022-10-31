a89802f8b27e361bb2e79d7770e5c9b632ed09f7
Idriss Neumann <idriss.neumann@comwork.io>
2022-10-31 17:19:27 +0100

    Fix: python method

--- utils/uprodit.py
+++ utils/uprodit.py
@@ -1,25 +1,25 @@
 from hashlib import sha1
-from urllib.parse import quote
 from uuid import uuid4
 from time import time
+from base64 import encodebytes
+from urllib.parse import quote
 
 import hmac
 import re
 
 def hmacsha1(key, raw):
-    hashed = hmac.new(key, raw, sha1)
-    return hashed.digest().encode("base64").rstrip('\n')
+    hashed = hmac.new(str.encode(key), str.encode(raw), sha1)
+    return encodebytes(hashed.digest()).decode('utf-8').rstrip('\n')
 
 def generate_signature (appid, env, uri):
     auth_signature_method = 'HMAC-SHA1'
-    auth_consumer_key = hmacsha1(appid, env)
+    auth_consumer_key = quote(hmacsha1(appid, env))
     auth_token = uuid4()
     uri_path = re.sub("http(s)?://[^/]*", "", uri)
-    auth_signature = quote(hmacsha1(appid, uri_path + auth_token))
-    auth_nonce = quote(hmacsha1(appid, uuid4()))
+    auth_signature = quote(hmacsha1(appid, "{}{}".format(uri_path, auth_token)))
+    auth_nonce = quote(hmacsha1(appid, "{}".format(uuid4())))
     auth_callback = quote(uri_path)
     auth_timestamp = time()
     return "Auth ?auth_signature={}&auth_nonce={}&auth_callback={}&auth_timestamp={}&auth_token={}&auth_signature_method={}&auth_consumer_key={}".format(auth_signature, auth_nonce, auth_callback, auth_timestamp, auth_token, auth_signature_method, auth_consumer_key)
 
 print(generate_signature("challenge_uprodit", "production", "https://api.uprodit.com/v2/profile/personal/en/51"))
-
