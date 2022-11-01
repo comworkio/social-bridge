995b5f980986dd21277ae7e9a2d8d5d6909600ac
Idriss Neumann <idriss.neumann@comwork.io>
2022-11-01 08:43:57 +0100

    Change keyword

--- ci/compute-env.sh
+++ ci/compute-env.sh
@@ -3,7 +3,7 @@
 export LOG_LEVEL=INFO
 export WAIT_TIME=60
 export KEYWORD_WAIT_TIME=2
-export TWITTER_MAX_RESULTS=20
+export TWITTER_MAX_RESULTS=100
 export TWITTER_KEYWORD_1=techwatch
 export TWITTER_USERNAME_1=IdrissNeumann
 export TWITTER_USERNAME_2=comworkio
@@ -21,4 +21,4 @@ export TWITTER_RETENTION_DAYS=2
 
 export CROSS_SLACK_TRIGGER=off
 export CROSS_TWITTER_USERNAME_1=IdrissNeumann
-export CROSS_TWITTER_KEYWORD_1=cross
+export CROSS_TWITTER_KEYWORD_1=mirrortw
