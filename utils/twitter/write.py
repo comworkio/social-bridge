a24ea1b6869c60216f52e2b04c454255d49ecb90
Idriss Neumann <idriss.neumann@comwork.io>
2022-11-23 15:14:41 +0100

    Add try/except on tweet method

--- utils/twitter/write.py
+++ utils/twitter/write.py
@@ -1,10 +1,13 @@
 import os
 from utils.common import is_from_another_account, sn_message
-from utils.logger import quiet_log_msg
+from utils.logger import log_msg, quiet_log_msg
 
 from utils.twitter.common import _TWITTER_CLIENT, is_twitter_enabled
 
 def tweet(username, message):
     if is_twitter_enabled() and None != _TWITTER_CLIENT and not is_from_another_account(message):
         quiet_log_msg("DEBUG", "[twitter][tweet] Send message from {} : {}".format(username, message))
-        _TWITTER_CLIENT.create_tweet(text = sn_message(username, message))
+        try:
+            _TWITTER_CLIENT.create_tweet(text = sn_message(username, message))
+        except Exception as e:
+            log_msg("WARN", "[twitter][tweet] unexpected error : {}".format(e))
